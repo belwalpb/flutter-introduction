b9f88402a83cb1aecba7687642a595320e240e70
Priyanshu Belwal <priyanshubelwalpb@gmail.com>
2022-10-09 17:09:36 +0530

    Day10 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,11 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My Application"),
+        backgroundColor: Colors.white,
+        elevation: 1.0,
+        iconTheme: const IconThemeData(color: Colors.black),
+        title: const Center(
+            child: Text("Catalog App", style: TextStyle(color: Colors.black))),
       ),
       body: Center(child: Text("Welcome $name at $days days of Flutter $pi")),
       drawer: const ApplicationDrawer(),
